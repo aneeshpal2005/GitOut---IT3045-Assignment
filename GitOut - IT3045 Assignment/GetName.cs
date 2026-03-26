717d3a8596a6d3d81c5f9d3b26c43bf084ceecf8
Aneesh Palande <45935068+aneeshpal2005@users.noreply.github.com>
2026-03-26 12:34:20 -0400

    Updated GetName to replace weatherforecast stuff with the name.

--- GitOut - IT3045 Assignment/GetName.cs
+++ GitOut - IT3045 Assignment/GetName.cs
@@ -2,12 +2,6 @@ namespace GitOut___IT3045_Assignment
 {
     public class GetName
     {
-        public DateOnly Date { get; set; }
-
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
+        public string Name { get; set; }
     }
 }
